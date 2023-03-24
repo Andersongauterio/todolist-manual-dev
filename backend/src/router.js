6b7defc2e8a1f0af86623939b7cb1177c169049e
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-03-24 20:10:02 -0300

    Middleware implementation

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
+
 const taskController = require('./controllers/taskController');
+const taskMiddleware = require('./middlewares/taskMiddleware');
 
 const router = express.Router();
 
 router.get('/tasks', taskController.getAll);
-router.post('/tasks', taskController.createTask);
+router.post('/tasks', taskMiddleware.validateBody, taskController.createTask);
 
 module.exports = router;
