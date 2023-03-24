018ce95eb66450a9077dc72f54f1ef82c2a9fdfd
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-03-23 21:10:26 -0300

    create task

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,12 +1,18 @@
 const taskModel = require('../models/taskModel');
 
-const getAll = async (request, response) => {
+const getAll = async (_request, response) => {
   
   const tasks = await taskModel.getAll();
 
   return response.status(200).json(tasks);
 };
 
+const createTask = async (request, response) => {
+  const createdTask = await taskModel.createTask(request.body);
+  return response.status(201).json(createdTask);
+};
+
 module.exports = {
-  getAll
+  getAll,
+  createTask,
 };
